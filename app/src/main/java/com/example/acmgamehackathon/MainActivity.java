2c783921455c23316a7ec15dd605b55af9fdd68a
EmilyPascua <epascuacollege@gmail.com>
2019-03-16 12:36:22 -0700

    working on menu

--- app/src/main/java/com/example/acmgamehackathon/MainActivity.java
+++ app/src/main/java/com/example/acmgamehackathon/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.acmgamehackathon;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -9,16 +12,27 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase mDatabase;
     private DatabaseReference mReference;
+    private ImageView menuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mDatabase = FirebaseDatabase.getInstance();
-        mReference = mDatabase.getReference();
-
-        mReference.setValue("test");
+//        mDatabase = FirebaseDatabase.getInstance();
+//        mReference = mDatabase.getReference();
+//
+//        mReference.setValue("test");
 
         setContentView(R.layout.activity_main);
+        menuItem.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                int id = view.getId();
+                Toast.makeText(getApplicationContext(), id + " ", Toast.LENGTH_LONG).show();
+            }
+
+        });
+
     }
 }
