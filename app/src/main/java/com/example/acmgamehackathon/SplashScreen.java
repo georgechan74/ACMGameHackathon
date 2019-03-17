376eda7e0395ecb1774061687368bbdfb60db0e7
treblegni <treblegni.longevity@gmail.com>
2019-03-17 10:04:14 -0700

    fixed syntax errors

--- app/src/main/java/com/example/acmgamehackathon/SplashScreen.java
+++ app/src/main/java/com/example/acmgamehackathon/SplashScreen.java
@@ -28,7 +28,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreen.this, GameSession.class);
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(i);
                 finish();
             }
