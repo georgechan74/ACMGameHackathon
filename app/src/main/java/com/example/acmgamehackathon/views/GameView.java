322f32f64ab1871a6a161dbfd512faf27d259c25
treblegni <treblegni.longevity@gmail.com>
2019-03-16 18:25:27 -0700

    latest push

--- app/src/main/java/com/example/acmgamehackathon/views/GameView.java
+++ app/src/main/java/com/example/acmgamehackathon/views/GameView.java
@@ -8,20 +8,22 @@ import android.view.SurfaceView;
 
 import com.example.acmgamehackathon.MainThread;
 
-import com.example.acmgamehackathon.model.Table;
+import com.example.acmgamehackathon.sprites.Table;
 import com.example.acmgamehackathon.sprites.BallSprite;
 
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
     private Table table;
     private BallSprite ball;
+    private int height = Resources.getSystem().getDisplayMetrics().heightPixels;
+    private int width = Resources.getSystem().getDisplayMetrics().widthPixels;
 
-    public GameView(Context context) {
+    public GameView(Context context,int screenWidth,int screenHeight) {
         super(context);
         getHolder().addCallback(this);
         thread = new MainThread(getHolder(), this);
-        table = new Table(Resources.getSystem().getDisplayMetrics().heightPixels,Resources.getSystem().getDisplayMetrics().heightPixels);
-        ball = new BallSprite();
+        table = new Table(height,width);
+        ball = new BallSprite(screenWidth,screenHeight);
         setFocusable(true);
     }
 
@@ -56,8 +58,26 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         super.draw(canvas);
 
         table.draw(canvas);
+        ball.update(30);
         ball.draw(canvas);
     }
 
-    public void update(){};
+    public void update(){
+        if (ball.getX() < 0) {
+            ball.reverseXVelocity();
+            ball.clearObstacleX(2);
+        }
+        else if (ball.getX() > width) {
+            ball.reverseXVelocity();
+            ball.clearObstacleX(width - 22);
+        }
+        if (ball.getY() < 0) {
+            ball.reverseYVelocity();
+            ball.clearObstacleY(16);
+        }
+        else if (ball.getX() > height) {
+            ball.reverseYVelocity();
+            ball.clearObstacleY(height - 2);
+        }
+    };
 }
